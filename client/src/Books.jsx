d72e024c6da69d80b0e27d08889b154c00c64aa8
Idiat Ameen <idi@idiats-air.home>
2020-06-13 15:08:00 -0400

    styling

--- client/src/Books.jsx
+++ client/src/Books.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import "./Books.css";
 import { Link, Route } from "react-router-dom";
 export default function Books(props) {
   const { books } = props;
 
   return (
     <>
+      <div className="book-club">
+        <h2>The Book Club</h2>
+      </div>
       {books.map((book) => (
         <React.Fragment key={book.title}>
-          <p>{book.title}</p>
+          <div className="books">
+            <p>{book.title}</p>
 
-          <img src={book.image} alt={book.title} />
-          <Link to={"/books/" + book.title}>View Book</Link>
+            <img src={book.image} alt={book.title} />
+            <div className="viewbook">
+              <Link to={"/books/" + book.title} className="text">
+                View
+              </Link>
+            </div>
+          </div>
         </React.Fragment>
       ))}
     </>
--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -18,62 +18,69 @@ export default class SignUp extends Component {
     const { username, email, password } = this.state;
     const { handleSignUpSubmit, history } = this.props;
     return (
-      <div className="page">
-        <form
-          className="register-box"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSignUpSubmit(this.state);
-            // history.push("/");
-            //redirect
-            this.setState({
-              username: "",
-              email: "",
-              password: "",
-            });
-          }}
-        >
+      <>
+        <div className="title">
           <h1>Welcome to The Clubs</h1>
-          <div className="credientials">
-            <label htmlFor="username">
-              username:
-              <input
-                id="username"
-                type="text"
-                name="username"
-                value={username}
-                onChange={this.handleChange}
-              />
-            </label>
-            <br />
-            <label htmlFor="email">
-              email:
-              <input
-                id="email"
-                type="text"
-                name="email"
-                value={email}
-                onChange={this.handleChange}
-              />
-            </label>
-            <br />
-            <label htmlFor="password">
-              password:
-              <input
-                id="password"
-                type="password"
-                name="password"
-                value={password}
-                onChange={this.handleChange}
-              />
-            </label>
-            <br />
-          </div>
-          <button>Submit</button>
-        </form>
-        <p>Already have an Account?</p>
-        <Link to="/signin">Sign In</Link>
-      </div>
+        </div>
+        <div className="page">
+          <div className="join-us">Join the Clubs</div>
+          <form
+            className="register-box"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSignUpSubmit(this.state);
+              // history.push("/");
+              //redirect
+              this.setState({
+                username: "",
+                email: "",
+                password: "",
+              });
+            }}
+          >
+            <div className="credientials">
+              <label htmlFor="username">
+                <input
+                  id="username"
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <label htmlFor="email">
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <label htmlFor="password">
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+            </div>
+            <button className="submit">Submit</button>
+          </form>
+          <p>Already have an Account?</p>
+          <Link to="/signin" className="link">
+            Sign In
+          </Link>
+        </div>
+      </>
     );
   }
 }
