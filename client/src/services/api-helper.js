aff760e2263b6ae539da0a286e15334f3d440ae4
Idiat Ameen <idi@idiats-air.home>
2020-06-16 00:31:44 -0400

    debug

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = "https://murmuring-wildwood-16683.herokuapp.com/";
-const baseUrl = "https:localhost:3000";
+const baseUrl = "https://murmuring-wildwood-16683.herokuapp.com/";
+
 const api = axios.create({
   baseURL: baseUrl,
 });
